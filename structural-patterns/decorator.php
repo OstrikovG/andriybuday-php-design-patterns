a687c00f34c5eaa503f4489200bf9cbb59ff1f64
OstrikovG <ostgleal@gmail.com>
2023-04-30 01:56:13 +0300

    finish decorator

--- structural-patterns/decorator.php
+++ structural-patterns/decorator.php
@@ -1,1 +1,47 @@
 <?php
+
+abstract class Car
+{
+    protected string $brandName;
+
+    public function go()
+    {
+        print("I'm " . $this->brandName . " and I'm on my way...\n");
+    }
+}
+
+// Конкретна реалізація класу Car
+class Mercedes extends Car
+{
+    public function __construct()
+    {
+        $this->brandName = "Mercedes";
+    }
+}
+
+class DecoratorCar extends Car
+{
+    protected Car $decoratedCar;
+
+    public function __construct(Car $decoratedCar)
+    {
+        $this->decoratedCar = $decoratedCar;
+    }
+
+    public function go()
+    {
+        $this->decoratedCar->go();
+    }
+}
+
+class AmbulanceCar extends DecoratorCar
+{
+    public function go()
+    {
+        parent::go();
+        print("... beep-beep-beeeeeep ...\n");
+    }
+}
+
+$doctorsDream = new AmbulanceCar(new Mercedes());
+$doctorsDream->go();
