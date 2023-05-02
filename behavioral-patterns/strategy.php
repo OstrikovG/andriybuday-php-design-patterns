28b011a698f70b945445bdbe2e5204cdc41f381f
OstrikovG <ostgleal@gmail.com>
2023-05-03 02:22:03 +0300

    finish strategy

--- behavioral-patterns/strategy.php
+++ behavioral-patterns/strategy.php
@@ -1,1 +1,72 @@
 <?php
+
+class MySelf
+{
+    private IWearingStrategy $wearingStrategy;
+
+    public function __construct()
+    {
+        $this->wearingStrategy = new DefaultWearingStrategy();
+    }
+
+    public function changeStrategy(IWearingStrategy $wearingStrategy): void
+    {
+        $this->wearingStrategy = $wearingStrategy;
+    }
+
+    public function goOutside()
+    {
+        $clothes = $this->wearingStrategy->getClothes();
+        $accessories = $this->wearingStrategy->getAccessories();
+        printf("Today I wore %s and took %s", $clothes, $accessories);
+    }
+}
+
+interface IWearingStrategy
+{
+    public function getClothes(): string;
+    public function getAccessories(): string;
+}
+
+class DefaultWearingStrategy implements IWearingStrategy
+{
+    public function getClothes(): string
+    {
+        return "Shirt";
+    }
+
+    public function getAccessories(): string
+    {
+        return "nothing";
+    }
+}
+
+class RainWearingStrategy implements IWearingStrategy
+{
+    public function getClothes(): string
+    {
+        return "Coat";
+    }
+
+    public function getAccessories(): string
+    {
+        return "umbrella";
+    }
+}
+
+class SunshineWearingStrategy implements IWearingStrategy
+{
+    public function getClothes(): string
+    {
+        return "T-Shirt";
+    }
+
+    public function getAccessories(): string
+    {
+        return "sunglasses";
+    }
+}
+
+$me = new MySelf();
+$me->changeStrategy(new RainWearingStrategy());
+$me->goOutside();
